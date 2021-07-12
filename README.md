0355bb104c3517fa02457e86993efb1e980beb7f
iidzim9 <52450700+iidzim9@users.noreply.github.com>
2021-07-12 18:04:12 +0100

    notion

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # philosophers
+
+## notions :
+ [https://www.notion.so/ikram9/Philosophers-766005d0b6cd4d5181b1f1b941587bde](https://www.notion.so/ikram9/Philosophers-766005d0b6cd4d5181b1f1b941587bde)
---
+++ philo.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iidzim <iidzim@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/12 11:05:10 by iidzim            #+#    #+#             */
+/*   Updated: 2021/07/12 11:08:06 by iidzim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <sys/time.h>
+
+
+
+#endif
