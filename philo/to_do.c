6e5385b13acb1b07bc7771134e7614be65fbcb07
Ikram Idzim <iidzim@e1r1p10.1337.ma>
2021-09-17 09:50:24 +0100

    case: ±5

---
+++ philo/to_do.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   to_do.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iidzim <iidzim@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/16 19:41:25 by iidzim            #+#    #+#             */
+/*   Updated: 2021/09/16 13:45:50 by iidzim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	get_forks(t_philo *p)
+{
+	int	n;
+
+	n = (p->id + 1) % p->data->nbr_philo;
+	if ((p->id % 2) == 0)
+	{
+		pthread_mutex_lock(&(p->data->forks[p->id]));
+		print_state(p, FORK);
+		pthread_mutex_lock(&(p->data->forks[n]));
+		print_state(p, FORK);
+	}
+	else
+	{
+		pthread_mutex_lock(&(p->data->forks[n]));
+		print_state(p, FORK);
+		pthread_mutex_lock(&(p->data->forks[p->id]));
+		print_state(p, FORK);
+	}
+}
+
+void	philo_eat(t_philo *p)
+{
+	int	n;
+
+	pthread_mutex_lock(&(p->eat));
+	p->last_time_eat = gettime();
+	p->is_eating = 1;
+	p->nbr_time_eat += 1;
+	pthread_mutex_unlock(&(p->eat));
+	print_state(p, EAT);
+	ft_usleep(p->data->time_to_eat);
+	n = (p->id + 1) % p->data->nbr_philo;
+	pthread_mutex_unlock(&(p->data->forks[p->id]));
+	pthread_mutex_unlock(&(p->data->forks[n]));
+	pthread_mutex_lock(&(p->eat));
+	p->is_eating = 0;
+	pthread_mutex_unlock(&(p->eat));
+}
+
+void	philo_sleep(t_philo *p)
+{
+	print_state(p, SLEEP);
+	ft_usleep(p->data->time_to_sleep);
+}
+
+void	philo_think(t_philo *p)
+{
+	print_state(p, THINK);
+}
---
+++ philo/utils.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iidzim <iidzim@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/13 10:41:20 by iidzim            #+#    #+#             */
+/*   Updated: 2021/09/16 13:45:53 by iidzim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	ft_atoi(const char *str)
+{
+	int		signe;
+	long	x;
+
+	signe = 1;
+	x = 0;
+	while (*str != '\0' && (*str == 32 || (*str >= 9 && *str <= 13)))
+		str++;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			signe = -1;
+		str++;
+	}
+	while (*str != '\0' && *str >= 48 && *str <= 57)
+	{
+		x = (x * 10) + ((char)*str - '0');
+		if (x > 4294967296)
+		{
+			if (signe > 0)
+				return (-1);
+			return (0);
+		}
+		str++;
+	}
+	return (x * signe);
+}
+
+int	ft_isdigit(char	*s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+			i++;
+		else
+			return (0);
+	}
+	return (1);
+}
+
+unsigned long long	gettime(void)
+{
+	struct timeval		current_time;
+	unsigned long long	time;
+
+	gettimeofday(&current_time, NULL);
+	time = current_time.tv_sec * 1000 + current_time.tv_usec / 1000;
+	return (time);
+}
+
+int	ft_usleep(int time)
+{
+	unsigned long long	curr_time;
+
+	curr_time = gettime();
+	while ((int)(gettime() - curr_time) < time)
+		usleep(10);
+	return (0);
+}
+
+int	print_state(t_philo *p, int i)
+{
+	pthread_mutex_lock(&(p->data->lock));
+	if (i == 0)
+		printf("%llu\t%d is eating\n", gettime() - p->time, p->id + 1);
+	else if (i == 1)
+		printf("%llu\t%d is sleeping\n", gettime() - p->time, p->id + 1);
+	else if (i == 2)
+		printf("%llu\t%d is thinking\n", gettime() - p->time, p->id + 1);
+	else if (i == 3)
+	{
+		printf("%llu\t%d died\n", gettime() - p->time, p->id + 1);
+		return (0);
+	}
+	else
+		printf("%llu\t%d has taken a fork\n", gettime() - p->time, p->id + 1);
+	pthread_mutex_unlock(&(p->data->lock));
+	return (1);
+}
